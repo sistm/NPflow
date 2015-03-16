0c2008a6a6a036c29c34e82b5ea8b44d07b8f099
borishejblum <boris.hejblum@isped.u-bordeaux2.fr>
2015-03-16 14:19:18 +0100

    examples: fix hyperprior specification

--- R/DPMGibbsSkewN_parallel.R
+++ R/DPMGibbsSkewN_parallel.R
@@ -92,7 +92,7 @@
 #' hyperG0[["kappa"]] <- 0.001
 #' hyperG0[["D_xi"]] <- 100
 #' hyperG0[["D_psi"]] <- 100
-#' hyperG0[["nu"]] <- d+0.1
+#' hyperG0[["nu"]] <- d + 1
 #' hyperG0[["lambda"]] <- diag(d)/10
 #'  
 #'  # hyperprior on the Scale parameter of DPM
