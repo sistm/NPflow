42dd43645ecbeef5a165885cf8cb2fd8d5a8365e
borishejblum <bhejblum.hsph@gmail.com>
2016-01-12 19:13:52 -0500

    slight speed up for the heatmap plotting

--- R/summary.DPMMclust.R
+++ R/summary.DPMMclust.R
@@ -35,64 +35,66 @@
 #'
 #'@importFrom gplots heatmap.2
 #'
+#'@importFrom fastcluster hclust
+#'
 #'@seealso \link{similarityMat}
 #'
 
 summary.DPMMclust <- function(x, burnin=0, thin=1, gs=NULL, lossFn="F-measure", K=10,
                               posterior_approx=FALSE, tol=2, maxit=50,dist="Normal",lambda=0,a=1,b=1){
-    
-    x_invar <- burn.DPMMclust(x, burnin = burnin, thin=thin, dist=dist)
-    
-    
-    
-    #if(!posterior_approx){
-    if(lossFn == "F-measure"){
-        point_estim <- cluster_est_Fmeasure(x_invar$mcmc_partitions, 
-                                            logposterior = sapply(x_invar$logposterior_list, sum))
-    }else if(lossFn == "Binder"){
-        point_estim <- cluster_est_binder(x_invar$mcmc_partitions, 
-                                          logposterior = sapply(x_invar$logposterior_list, sum))
-    }else if(lossFn == "MBinderN"){
-      if (dist!="Normal"){stop("Clusters distribution must be Gaussian")}
-      point_estim <- cluster_est_MBinderN(x_invar$mcmc_partitions,x_invar$listU_mu,
-                                     x_invar$listU_Sigma,lambda,a,b,
-                                     logposterior = sapply(x_invar$logposterior_list, sum))
-    }else{
-        stop("Specified loss function not available.\n 
+  
+  x_invar <- burn.DPMMclust(x, burnin = burnin, thin=thin, dist=dist)
+  
+  
+  
+  #if(!posterior_approx){
+  if(lossFn == "F-measure"){
+    point_estim <- cluster_est_Fmeasure(x_invar$mcmc_partitions, 
+                                        logposterior = sapply(x_invar$logposterior_list, sum))
+  }else if(lossFn == "Binder"){
+    point_estim <- cluster_est_binder(x_invar$mcmc_partitions, 
+                                      logposterior = sapply(x_invar$logposterior_list, sum))
+  }else if(lossFn == "MBinderN"){
+    if (dist!="Normal"){stop("Clusters distribution must be Gaussian")}
+    point_estim <- cluster_est_MBinderN(x_invar$mcmc_partitions,x_invar$listU_mu,
+                                        x_invar$listU_Sigma,lambda,a,b,
+                                        logposterior = sapply(x_invar$logposterior_list, sum))
+  }else{
+    stop("Specified loss function not available.\n 
              Specify either 'F-measure' or 'Binder' for the lossFn argument.")
+  }
+  
+  index_estim <- point_estim$opt_ind
+  loss <- NA
+  if(!is.null(gs)){
+    loss <- evalClustLoss(c=point_estim$c_est, gs=gs, lossFn=lossFn)
+  }
+  #}
+  #point_estim=NULL
+  #index_estim <- NA
+  #loss <- NA
+  #Posterior approximation
+  
+  if(posterior_approx){
+    if(K>1){
+      param_post <- postProcess.DPMMclust(x_invar, plot=FALSE, tol=tol, K=K, maxit=maxit)
+    }else{
+      param_post <- postProcess.DPMMclust(x_invar, K=1)
     }
-    
-    index_estim <- point_estim$opt_ind
-    loss <- NA
-    if(!is.null(gs)){
-        loss <- evalClustLoss(c=point_estim$c_est, gs=gs, lossFn=lossFn)
-    }
-    #}
-    #point_estim=NULL
-    #index_estim <- NA
-    #loss <- NA
-    #Posterior approximation
-    
-    if(posterior_approx){
-        if(K>1){
-            param_post <- postProcess.DPMMclust(x_invar, plot=FALSE, tol=tol, K=K, maxit=maxit)
-        }else{
-            param_post <- postProcess.DPMMclust(x_invar, K=1)
-        }
-    }
-    else{
-        param_post <- NULL
-    }
-    
-    s <- c(x_invar, list("burnin"=burnin,
-                         "thin"=thin,
-                         "point_estim"=point_estim,
-                         "loss"=loss,
-                         "index_estim"=index_estim,
-                         "param_posterior"=param_post))
-    class(s) <- "summaryDPMMclust"
-    
-    invisible(s)
+  }
+  else{
+    param_post <- NULL
+  }
+  
+  s <- c(x_invar, list("burnin"=burnin,
+                       "thin"=thin,
+                       "point_estim"=point_estim,
+                       "loss"=loss,
+                       "index_estim"=index_estim,
+                       "param_posterior"=param_post))
+  class(s) <- "summaryDPMMclust"
+  
+  invisible(s)
 }
 
 
@@ -108,81 +110,83 @@ summary.DPMMclust <- function(x, burnin=0, thin=1, gs=NULL, lossFn="F-measure", 
 #'@author Boris Hejblum
 #'@export
 print.summaryDPMMclust <- function(s,...){
-    
-    cat(class(s), "object with", s$nb_mcmcit, "MCMC iterations:\n")
-    cat(rep("-",40),"\n", sep="")
-    cat(rep("-",40),"\n", sep="")
-    cat("Burnin =", s$burnin, "iterations\n\n")
-    cat("Point estimate of the partition with a ", s$clust_distrib," mixture:\n", sep="")
-    t<-table(s$point_estim$c_est)
-    t2print <- paste(formatC(as.vector(t/length(s$point_estim$c_est)), 
-                             digits=2),"%", sep="")
-    names(t2print) <- names(t)
-    print(t2print, quote=F)
-    cat("\nLoss of the point estimate partition:", s$loss, "\n")
-    cat(rep("-",40),"\n", sep="")
-    
+  
+  cat(class(s), "object with", s$nb_mcmcit, "MCMC iterations:\n")
+  cat(rep("-",40),"\n", sep="")
+  cat(rep("-",40),"\n", sep="")
+  cat("Burnin =", s$burnin, "iterations\n\n")
+  cat("Point estimate of the partition with a ", s$clust_distrib," mixture:\n", sep="")
+  t<-table(s$point_estim$c_est)
+  t2print <- paste(formatC(as.vector(t/length(s$point_estim$c_est)), 
+                           digits=2),"%", sep="")
+  names(t2print) <- names(t)
+  print(t2print, quote=F)
+  cat("\nLoss of the point estimate partition:", s$loss, "\n")
+  cat(rep("-",40),"\n", sep="")
+  
 }
 
 #'@export
 #'@rdname methods.summaryDPMMclust
 plot.summaryDPMMclust <- function(s, hm=FALSE, nbsim_densities=5000, gg.add=list(theme_bw()),...){
-    if(length(s$logposterior_list[[1]])>1){
-        plot_ConvDPM(s, shift=s$burnin)
+  if(length(s$logposterior_list[[1]])>1){
+    plot_ConvDPM(s, shift=s$burnin)
+  }
+  ind <- s$index_estim
+  
+  
+  cat("Plotting point estimate (may take a few sec)... ")
+  if(s$clust_distrib=="Normal"){
+    plot_DPM(z=s$data,
+             c=s$point_estim$c_est, 
+             i=ind+s$burnin, 
+             alpha=s$alpha[ind], 
+             U_SS=s$U_SS_list[[ind]], 
+             ellipses=TRUE,
+             gg.add=gg.add,
+             ...
+    )
+  }else if(s$clust_distrib=="skewNormal"){
+    plot_DPMsn(z=s$data,
+               c=s$point_estim$c_est, 
+               i=ind+s$burnin, 
+               alpha=s$alpha[ind], 
+               U_SS=s$U_SS_list[[ind]], 
+               ellipses=TRUE,
+               gg.add= gg.add,
+               nbsim_dens=nbsim_densities,
+               ...
+    )
+  }else if(s$clust_distrib=="skewT"){
+    plot_DPMst(z=s$data,
+               c=s$point_estim$c_est, 
+               i=ind+s$burnin, 
+               alpha=s$alpha[ind], 
+               U_SS=s$U_SS_list[[ind]], 
+               ellipses=TRUE,
+               gg.add=gg.add,
+               nbsim_dens=nbsim_densities,
+               nice=TRUE,
+               ...
+    )
+  }
+  cat("DONE!\n")
+  
+  if(hm){
+    cat("Plotting heatmap of similarity (may take a few min)...\n")
+    if(is.null(s$point_estim$similarity)){
+      stop("In order to plot the similarity matrix, the 'Binder' loss function should be used")
     }
-    ind <- s$index_estim
-    
-    
-    cat("Plotting point estimate (may take a few sec)... ")
-    if(s$clust_distrib=="Normal"){
-        plot_DPM(z=s$data,
-                 c=s$point_estim$c_est, 
-                 i=ind+s$burnin, 
-                 alpha=s$alpha[ind], 
-                 U_SS=s$U_SS_list[[ind]], 
-                 ellipses=TRUE,
-                 gg.add=gg.add,
-                 ...
-        )
-    }else if(s$clust_distrib=="skewNormal"){
-        plot_DPMsn(z=s$data,
-                   c=s$point_estim$c_est, 
-                   i=ind+s$burnin, 
-                   alpha=s$alpha[ind], 
-                   U_SS=s$U_SS_list[[ind]], 
-                   ellipses=TRUE,
-                   gg.add= gg.add,
-                   nbsim_dens=nbsim_densities,
-                   ...
-        )
-    }else if(s$clust_distrib=="skewT"){
-        plot_DPMst(z=s$data,
-                   c=s$point_estim$c_est, 
-                   i=ind+s$burnin, 
-                   alpha=s$alpha[ind], 
-                   U_SS=s$U_SS_list[[ind]], 
-                   ellipses=TRUE,
-                   gg.add=gg.add,
-                   nbsim_dens=nbsim_densities,
-                   nice=TRUE,
-                   ...
-        )
-    }
-    cat("DONE!\n")
-    
-    if(hm){
-        if(is.null(s$point_estim$similarity)){
-            stop("In order to plot the similarity matrix, the 'Binder' loss function should be used")
-        }
-        cat("Plotting heatmap of similarity (may take a few min)...\n")
-        pheatmap(s$point_estim$similarity, scale="none", border_color=NA,
-                 color=colorRampPalette(c("#F7FBFF", "#DEEBF7", "#C6DBEF", 
-                                          #"#9ECAE1", "#FEB24C", 
-                                          "#FD8D3C", "#BD0026", "#800026"))(200), 
-                 show_rownames=FALSE, show_colnames=FALSE, 
-                 cluster_rows=TRUE, cluster_cols =TRUE, 
-                 main="Posterior similarity matrix")
-        cat("DONE! Wait for plot rendering...\n")
-    }
-    
+    tree <- fastcluster::hclust(dist(s$point_estim$similarity, method = "euclidean"), 
+                                method = "complete")
+    ord_index <- tree$order
+    pheatmap(s$point_estim$similarity[ord_index, ord_index], scale="none", border_color=NA,
+             color=colorRampPalette(c("#F7FBFF", "#DEEBF7", "#C6DBEF", 
+                                      #"#9ECAE1", "#FEB24C", 
+                                      "#FD8D3C", "#BD0026", "#800026"))(200), 
+             show_rownames=FALSE, show_colnames=FALSE, 
+             cluster_rows=FALSE, cluster_cols =TRUE, 
+             main="Posterior similarity matrix")
+    cat("DONE! Wait for plot rendering...\n")
+  }
 }
