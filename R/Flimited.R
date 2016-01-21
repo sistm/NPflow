1941968720774b409a27b33ac21c104d5caa92c7
chariff <chariff.alkhassim@gmail.com>
2016-01-21 10:09:42 +0100

    Flimited typo fix
    as above

--- R/Flimited.R
+++ R/Flimited.R
@@ -17,27 +17,27 @@
 
 Flimited<-function(n_small_clst,partition_ref,partition_est){
   
-  partition_ref<-as.numeric(partition_ref)
-  partition_est<-as.numeric(partition_est)
+  partition_ref <- as.numeric(partition_ref)
+  partition_est <- as.numeric(partition_est)
   
-  label_obs_in_small_class_ref<-which(table(partition_ref)<=n_small_clst)
+  label_obs_in_small_class_ref <- which(table(partition_ref)<=n_small_clst)
   if(!length(label_obs_in_small_class_ref)){
     stop('Error : n_small_clst should be increased')
   }
   
   label_obs_in_small_class_ref <- as.numeric(names(label_obs_in_small_class_ref))
-  index_obs_in_small_clust_ref <- which(partition_ref %in% label_obs_in_small_class_ref)
-  label_obs_in_small_class_est <- unique(partition_est[index_obs_in_small_clust_ref]) 
+  index_obs_in_small_class_ref <- which(partition_ref %in% label_obs_in_small_class_ref)
+  label_obs_in_small_class_est <- unique(partition_est[index_obs_in_small_class_ref]) 
   
-  partition_est_restricted<-partition_est[partition_est%in%label_obs_in_small_class_est]
+  partition_est_restricted <- partition_est[partition_est%in%label_obs_in_small_class_est]
   
   
-  find_index_obs_coclust<-partition_est%in%label_obs_in_small_class_est
-  find_index_obs_coclust[index_obs_in_small_clust_ref]<-FALSE
-  index_obs_in_small_clust_ref_2<- which(find_index_obs_coclust==TRUE)
+  find_index_obs_coclust <- partition_est%in%label_obs_in_small_class_est
+  find_index_obs_coclust[index_obs_in_small_class_ref] <- FALSE
+  index_obs_in_small_class_ref_2 <- which(find_index_obs_coclust==TRUE)
   
-  partition_ref_restricted<-c(partition_ref[sort(c(index_obs_in_small_clust_ref,
-                                                   index_obs_in_small_clust_ref_2))])
+  partition_ref_restricted <- c(partition_ref[sort(c(index_obs_in_small_class_ref,
+                                                     index_obs_in_small_class_ref_2))])
   
   return(FmeasureC(ref=partition_ref_restricted, pred=partition_est_restricted))
 }
