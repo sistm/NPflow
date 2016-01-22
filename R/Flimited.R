5411186b221916a2d95dbc2d4b788d52ee1e45de
borishejblum <bhejblum.hsph@gmail.com>
2016-01-22 14:52:43 -0500

    Simplifying the Flimited implementation

--- R/Flimited.R
+++ R/Flimited.R
@@ -1,8 +1,10 @@
 #'Compute a limited F-measure
 #'
+#'A limited version of F-measure for
+#'
 #'@name Flimited
 #'
-#'@param n_small_clst
+#'@param n_small_clst the size of the 
 #'
 #'@param partition_ref
 #'
@@ -10,34 +12,24 @@
 #'
 #'
 #'
-#'@export Flimited
-#'
-#'
-#'
-
-Flimited<-function(n_small_clst,partition_ref,partition_est){
+#'@export
+Flimited <- function(n_small_clst, pred, ref){
   
-  partition_ref <- as.numeric(partition_ref)
-  partition_est <- as.numeric(partition_est)
+  partition_ref <- as.numeric(ref)
+  partition_est <- as.numeric(pred)
   
   label_obs_in_small_class_ref <- which(table(partition_ref)<=n_small_clst)
   if(!length(label_obs_in_small_class_ref)){
-    stop('Error : n_small_clst should be increased')
+    stop('n_small_clst should be increased')
   }
   
   label_obs_in_small_class_ref <- as.numeric(names(label_obs_in_small_class_ref))
   index_obs_in_small_class_ref <- which(partition_ref %in% label_obs_in_small_class_ref)
   label_obs_in_small_class_est <- unique(partition_est[index_obs_in_small_class_ref]) 
   
-  partition_est_restricted <- partition_est[partition_est%in%label_obs_in_small_class_est]
-  
-  
-  find_index_obs_coclust <- partition_est%in%label_obs_in_small_class_est
-  find_index_obs_coclust[index_obs_in_small_class_ref] <- FALSE
-  index_obs_in_small_class_ref_2 <- which(find_index_obs_coclust==TRUE)
-  
-  partition_ref_restricted <- c(partition_ref[sort(c(index_obs_in_small_class_ref,
-                                                     index_obs_in_small_class_ref_2))])
+  index_obs_restricted <- which(partition_est%in%label_obs_in_small_class_est)
+  partition_est_restricted <- partition_est[index_obs_restricted]
+  partition_ref_restricted <- partition_ref[index_obs_restricted]
   
   return(FmeasureC(ref=partition_ref_restricted, pred=partition_est_restricted))
 }
