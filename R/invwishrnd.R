663be61788714443bb7a2ddc05533902dbb4b259
chariff <chariff.alkhassim@gmail.com>
2016-03-30 17:39:37 +0200

    Handling of a degenerate lambda matrix

--- R/invwishrnd.R
+++ R/invwishrnd.R
@@ -10,8 +10,12 @@
 #'
 #'@export
 #'
+#'
 invwishrnd <- function(n,lambda){
-    iS=wishrnd(n = n, Sigma = solve(lambda))
-    S=solve(iS)
-    return(S)
+  p<-ncol(lambda)
+  S<-try(solve(wishrnd(n = n, Sigma = solve(lambda))),silent=TRUE)
+  if(inherits(S, "try-error")){
+    S=solve(wishrnd(n = n, Sigma = solve((lambda+diag(p)))))
+  }
+  return(S)
 }
