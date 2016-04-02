a4693890f528f25acbf7c0352260be2adadb2b16
borishejblum <bhejblum.hsph@gmail.com>
2016-04-02 14:36:01 -0400

    documentation update

--- R/NPflow-package.R
+++ R/NPflow-package.R
@@ -10,19 +10,18 @@
 #'Date: \tab 2016-02-03\cr
 #'License:\tab \href{http://www.gnu.org/licenses/lgpl.txt}{LGPL-3}\cr
 #'}
-#'The main function in this package is \code{\link{DPMgibbs}} which 
-#'performs TODO 
+#'The main function in this package is \code{\link{DPMgibbs}} which
+#'performs TODO
 #'
 #'@author Boris P. Hejblum
 #'--- Maintainer: Boris P. Hejblum
 #'
-#'@references Hejblum, BP, Alkhassim C, Gottardo R, Caron, F, Thiebaut, R, 2016, 
+#'@references Hejblum, BP, Alkhassim C, Gottardo R, Caron, F, Thiebaut, R, 2016,
 #'
 #'@docType package
 #'@name NPflow-package
 #'@aliases NPflow
 #'
-#'@import RcppArmadillo
 #'@useDynLib NPflow
 #'@importFrom Rcpp sourceCpp
 #'
