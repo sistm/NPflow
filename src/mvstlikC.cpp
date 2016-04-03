22f6cc990ec51e4605182f4fa47dcd33be697f5c
borishejblum <bhejblum.hsph@gmail.com>
2016-04-03 19:43:03 -0400

    mvstlikC arma direct

--- src/mvstlikC.cpp
+++ src/mvstlikC.cpp
@@ -6,84 +6,50 @@ using namespace arma;
 
 //' C++ implementation of multivariate Normal probability density function for multiple inputs
 //'
-//'@param x data matrix of dimension p x n, p being the dimension of the 
-//'data and n the number of data points 
+//'@param x data matrix of dimension p x n, p being the dimension of the
+//'data and n the number of data points
 //'@param c integer vector of cluster allocations with values from 1 to K
-//'@param clustval vector of unique values from c in the order corresponding to 
+//'@param clustval vector of unique values from c in the order corresponding to
 //'the storage of cluster parameters in \code{xi}, \code{psi}, and \code{varcovM}
-//'@param xi mean vectors matrix of dimension p x K, K being the number of 
+//'@param xi mean vectors matrix of dimension p x K, K being the number of
 //'clusters
 //'@param psi skew parameter vectors matrix of dimension p x K
-//'@param varcovM list of length K of variance-covariance matrices, 
+//'@param varcovM list of length K of variance-covariance matrices,
 //'each of dimensions p x p
 //'@param df vector of length K of degree of freedom parameters
-//'@return a list: "indiv": vector of likelihood of length n; "clust": vector of likelihood of length K; "total": total (log)-likelihood
-//'@seealso mmvstpdfC, mvstpdf
-//'@export
-//'@examples
-//'
-//'#skew-normal limit
-//'mmvsnpdfC(x=matrix(rep(1.96,2), nrow=2, ncol=1), 
-//'          xi=matrix(c(0, 0)), psi=matrix(c(1, 1),ncol=1), sigma=list(diag(2))
-//'          )
-//'mvstpdf(x=matrix(rep(1.96,2), nrow=2, ncol=1),
-//'        xi=c(0, 0), psi=c(1, 1), sigma=diag(2),
-//'        df=100000000
-//'        )
-//'mmvstpdfC(x=matrix(rep(1.96,2), nrow=2, ncol=1), 
-//'          xi=matrix(c(0, 0)), psi=matrix(c(1, 1),ncol=1), sigma=list(diag(2)),
-//'          df=100000000
-//'          )
-//'          
-//'#non-skewed limit         
-//'mmvtpdfC(x=matrix(rep(1.96,2), nrow=2, ncol=1),
-//'         mean=matrix(c(0, 0)), varcovM=list(diag(2)),
-//'         df=10
-//'         )
-//'mmvstpdfC(x=matrix(rep(1.96,2), nrow=2, ncol=1), 
-//'          xi=matrix(c(0, 0)), psi=matrix(c(0, 0),ncol=1), sigma=list(diag(2)),
-//'          df=10
-//'          )
-//'          
-//'microbenchmark(mvstpdf(x=matrix(rep(1.96,2), nrow=2, ncol=1), 
-//'                       xi=c(0, 0), psi=c(1, 1), 
-//'                       sigma=diag(2), df=10),
-//'               mmvstpdfC(x=matrix(rep(1.96,2), nrow=2, ncol=1),
-//'                         xi=matrix(c(0, 0)), psi=matrix(c(1, 1),ncol=1), 
-//'                         sigma=list(diag(2)), df=10),
-//'               times=10000L)
+//'@return a list:
+//'\itemize{
+//'\item{\code{"indiv"}:}{ vector of likelihood of length n;}
+//'\item{\code{"clust"}:}{ vector of likelihood of length K;}
+//'\item{\code{"total"}:}{ total (log)-likelihood;}
+//'}
 //'
 // [[Rcpp::export]]
-List mvstlikC(NumericMatrix x, 
-              IntegerVector c, 
-              IntegerVector clustval, 
-              NumericMatrix xi, 
-              NumericMatrix psi, 
-              List sigma, 
-              NumericVector df, 
+List mvstlikC(arma::mat x,
+              arma::vec c,
+              arma::vec clustval,
+              arma::mat xi,
+              arma::mat psi,
+              List sigma,
+              NumericVector df,
               bool loglik=true){
-    
-    mat xx = as<mat>(x);
-    mat mxi = as<mat>(xi); 
-    mat mpsi = as<mat>(psi);
-    vec cc = as<vec>(c);
-    vec cclustval = as<vec>(clustval);
-    int p = xx.n_rows;
-    int n = xx.n_cols;
-    int K = mxi.n_cols;
+
+    int p = x.n_rows;
+    int n = x.n_cols;
+    int K = xi.n_cols;
     vec yindiv = vec(n);
     vec yclust = vec(K);
-    
+
     for(int k=0; k < K; k++){
-        uvec indk = find(cc==cclustval(k));
-        mat xtemp= xx.cols(indk);
+        uvec indk = find(c==clustval(k));
+        mat xtemp= x.cols(indk);
         int ntemp = indk.size();
-        
-        colvec mtemp = mxi.col(k);
-        mat psitemp = mpsi.col(k);
+
+        colvec mtemp = xi.col(k);
+        mat psitemp = psi.col(k);
         mat sigmatemp = sigma[k];
         double dftemp = df(k);
-        
+
         mat omega = sigmatemp + psitemp*trans(psitemp);
         mat omegaInv =  inv_sympd(omega);
         mat Rinv=inv(trimatu(chol(omega)));
@@ -93,7 +59,7 @@ List mvstlikC(NumericMatrix x,
         vec alphden = rep(alphtemp(0,0), alphnum.size());
         vec alph = alphnum/alphden;
         double logSqrtDetvarcovM = sum(log(Rinv.diag()));
-        
+
         for (int i=0; i < ntemp; i++) {
             colvec x_i = xtemp.col(i) - mtemp;
             rowvec xRinv = trans(x_i)*Rinv;
@@ -108,17 +74,17 @@ List mvstlikC(NumericMatrix x,
         yclust(k) = sum(yindiv(indk));
     }
     double ytot = sum(yclust);
-    
+
     if(!loglik){
         yindiv = exp(yindiv);
         yclust = exp(yclust);
         ytot = exp(ytot);
     }
-    
-    
+
+
     return Rcpp::List::create(Rcpp::Named("indiv") = wrap(yindiv),
                               Rcpp::Named("clust") = wrap(yclust),
                               Rcpp::Named("total") = wrap(ytot)
                              );
-    
+
 }
