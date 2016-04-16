6c6fe3d3b3b525aa53e5ab2ea27d1d0f796e45e4
borishejblum <bhejblum.hsph@gmail.com>
2016-04-16 12:21:16 -0400

    updating random generation number in C++ according to best practices

--- src/sampleClassC.cpp
+++ src/sampleClassC.cpp
@@ -4,14 +4,14 @@ using namespace arma;
 
 // [[Rcpp::depends(RcppArmadillo)]]
 
-//' C++ implementation of the multinomial sampling from a matrix 
-//' of column vectors, each containing the sampling probabilities 
+//' C++ implementation of the multinomial sampling from a matrix
+//' of column vectors, each containing the sampling probabilities
 //' for their respective draw
-//' 
-//'@param probMat a numeric matrix of dim \code{k x n} of containing column vectors of sampling 
+//'
+//'@param probMat a numeric matrix of dim \code{k x n} of containing column vectors of sampling
 //'probabilities for each class \code{k}.
 //'
-//'@return a vector of integer of length \code{n} containing the multinomial draws for each 
+//'@return a vector of integer of length \code{n} containing the multinomial draws for each
 //'observation, i.e. the class allocation.
 //'
 //'@keywords internal
@@ -20,10 +20,12 @@ using namespace arma;
 //'
 // [[Rcpp::export]]
 IntegerVector sampleClassC(arma::mat probMat){
-    
+
     int n = probMat.n_cols;
     IntegerVector c = IntegerVector(n);
-    
+
+    GetRNGstate();
+
     for(int i=0; i<n; i++){
         vec pm_i = probMat.col(i);
         vec probs = pm_i/sum(pm_i);
@@ -31,6 +33,8 @@ IntegerVector sampleClassC(arma::mat probMat){
         uvec c_i = find(probs_cumsum > unif_rand());
         c(i) = c_i(0) + 1;
     }
-    
+
+    PutRNGstate();
+
     return(c);
 }
