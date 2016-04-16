ec53d5255dc712cd16d1bb5a48a857746256a14a
borishejblum <bhejblum.hsph@gmail.com>
2016-04-16 13:49:39 -0400

    one more try to get Flimited examples to run on Travis CI

--- R/Flimited.R
+++ R/Flimited.R
@@ -16,7 +16,7 @@
 #'library(NPflow)
 #'pred <- c(rep(1, 5),rep(2, 8),rep(3,10))
 #'ref <- c(rep(1, 5),rep(c(2,3), 4),rep(c(3,2),5))
-#'FmeasureC(pred, ref)
+#'NPflow::FmeasureC(pred, ref)
 #'Flimited(6, pred, ref)
 #'
 #'@export
