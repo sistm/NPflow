a5007fd89b97404eebc876db600de56a9271e9d0
borishejblum <bhehblum.hsph@gmail.com>
2016-07-06 09:58:16 -0400

    adding previous gating arg to cytoScatter

--- R/cytoScatter.R
+++ R/cytoScatter.R
@@ -6,6 +6,9 @@
 #'@param dims2plot a vector of length at least 2, indicating of the dimensions to be plotted.
 #'Default is \code{c(1, 2)}.
 #'
+#'@param gating an optional vector of length \code{n} indicating a known gating of the cells to be 
+#'displayed. Default is \code{NULL} in which case no gating is displayed.
+#'
 #'@param scale_log a logical Flag indicating wether the data should be plotted on the log scale.
 #'Default is \code{FALSE}.
 #'
@@ -50,113 +53,138 @@
 #' cytoScatter(z)
 #'
 #'@export
-cytoScatter <- function(cytomatrix, dims2plot=c(1,2),
-                      scale_log=FALSE, xlim=NULL, ylim=NULL, gg.add=list(theme())){
-
-    n <- ncol(cytomatrix)
-    stopifnot(length(dims2plot)>1)
-    stopifnot(is.matrix(cytomatrix))
-
-    if(length(dims2plot)!=2){
-        cytomatrix <- cytomatrix[dims2plot,]
-
-        zDplot <- melt(cbind.data.frame("ID"=as.character(1:n),
-                                        t(cytomatrix)
-        ),
-        id.vars=c("ID"),
-        variable.name = "dimensionX",
-        value.name="X"
-        )
-        zDplotfull <- zDplot
-        zDplotfull$Y <- zDplot$X
-        zDplotfull$dimensionY <- zDplot$dimensionX
-
-        lev <- as.character(1:length(levels(zDplot$dimensionX)))
-        for(l in 2:length(lev)){
-            move <- which(as.numeric(zDplot$dimensionX)<l)
-            zDplottemp <- rbind.data.frame(zDplot[-move,], zDplot[move,])
-            zDplottemp$Y <- zDplot$X
-            zDplottemp$dimensionY <- zDplot$dimensionX
-            zDplotfull <- rbind.data.frame(
-                zDplotfull, zDplottemp)
-        }
-
-        p <- (ggplot(zDplotfull, aes_string(x="X", y="Y"))
-              + facet_grid(dimensionY~dimensionX, scales="free")
-              + geom_point(colour="blue",
-                           data=zDplotfull, alpha=1, size=2/(0.3*log(n)))
-              + stat_density2d(aes_string(fill = "..level.."), alpha=0.8, geom="polygon")
-              + scale_fill_gradientn(colours=c("blue","green", "yellow", "red"),
-                                     name="Density")
-              + theme_bw()
-              + ggtitle(paste(n, " cells",
-                              sep=""))
-        )
-
+cytoScatter <- function(cytomatrix, dims2plot=c(1,2), gating=NULL,
+                        scale_log=FALSE, xlim=NULL, ylim=NULL, gg.add=list(theme())){
+  
+  n <- ncol(cytomatrix)
+  stopifnot(length(dims2plot)>1)
+  stopifnot(is.matrix(cytomatrix))
+  
+  
+  if(length(dims2plot)!=2){
+    cytomatrix <- cytomatrix[dims2plot,]
+    
+    zDplot <- melt(cbind.data.frame("ID"=as.character(1:n), t(cytomatrix)),
+                   id.vars=c("ID"),
+                   variable.name = "dimensionX",
+                   value.name="X"
+    )
+    zDplotfull <- zDplot
+    zDplotfull$Y <- zDplot$X
+    zDplotfull$dimensionY <- zDplot$dimensionX
+    
+    lev <- as.character(1:length(levels(zDplot$dimensionX)))
+    for(l in 2:length(lev)){
+      move <- which(as.numeric(zDplot$dimensionX)<l)
+      zDplottemp <- rbind.data.frame(zDplot[-move,], zDplot[move,])
+      zDplottemp$Y <- zDplot$X
+      zDplottemp$dimensionY <- zDplot$dimensionX
+      zDplotfull <- rbind.data.frame(
+        zDplotfull, zDplottemp)
+    }
+    
+    if(!is.null(gating)){
+      stopifnot(length(gating)==n)
+      zDplotfull <- cbind.data.frame(zDplotfull, "Gate"=rep(as.character(gating), nrow(zDplotfull)/length(gating)))
+    }
+    
+    if(is.null(gating)){
+      p <- (ggplot(zDplotfull, aes_string(x="X", y="Y"))
+            + facet_grid(dimensionY~dimensionX, scales="free")
+            + geom_point(colour="blue", data=zDplotfull, alpha=1, size=2/(0.3*log(n)))
+            + stat_density2d(aes_string(fill = "..level.."), alpha=0.8, geom="polygon")
+            + scale_fill_gradientn(colours=c("blue","green", "yellow", "red"),
+                                   name="Density")
+            + theme_bw()
+            + ggtitle(paste(n, " cells", sep=""))
+      )
     }else{
+      p <- (ggplot(zDplotfull, aes_string(x="X", y="Y", group="Gate"))
+            + facet_grid(dimensionY~dimensionX, scales="free")
+            #+ stat_density2d(aes_string(fill = "..level.."), alpha=0.1, geom="polygon")
+            + geom_point(aes_string(colour="Gate"), data=zDplotfull, alpha=0.4, size=2/(0.3*log(n)))      
+            + theme_bw()
+            + ggtitle(paste(n, " cells", sep=""))
+            + guides(colour=guide_legend(override.aes = list(alpha=1)))
+      )
+    }
+    
+  }else{
+    
+    if(is.character(dims2plot)){
+      if(length(which(dims2plot%in%rownames(cytomatrix)))!=2){
+        stop("'dims2plot' not in rownames of 'cytomatrix'")
+      }
+      dims2plot[1] <- which(rownames(cytomatrix)==dims2plot[1])
+      dims2plot[2] <- which(rownames(cytomatrix)==dims2plot[2])
+      dims2plot <- as.numeric(dims2plot)
+    }
+    
+    data2plot <- data.frame(t(cytomatrix[dims2plot,]))
+    if(!is.null(gating)){
+      stopifnot(length(gating)==n)
+      data2plot <- cbind.data.frame(data2plot, "Gate"=as.character(gating))
+    }
+    
+    if(is.null(colnames(data2plot)[1]) | is.null(colnames(data2plot)[1])){
+      colnames(data2plot)[1:2] <- c("X", "Y")
+    }
+    xname <- colnames(data2plot)[1]
+    yname <- colnames(data2plot)[2]
+    p <- ggplot(data2plot, aes_string(x=xname, y=yname))
 
-        if(is.character(dims2plot)){
-            if(length(which(dims2plot%in%rownames(cytomatrix)))!=2){
-                stop("'dims2plot' not in rownames of 'cytomatrix'")
-            }
-            dims2plot[1] <- which(rownames(cytomatrix)==dims2plot[1])
-            dims2plot[2] <- which(rownames(cytomatrix)==dims2plot[2])
-            dims2plot <- as.numeric(dims2plot)
+    if(is.null(gating)){
+    p <- (p + geom_point(size=2/(0.3*log(n)), colour="blue")
+          + stat_density2d(aes_string(fill = "..level.."), alpha=0.8, geom="polygon")
+          + scale_fill_gradientn(colours=c("blue","green", "yellow", "red"),
+                                 name="Density")
+          + theme_bw()
+          + ggtitle(paste(n, " cells", sep=""))
+    )
+    }else{
+      p <- (p #+ stat_density2d(aes_string(fill = "..level.."), alpha=0.1, geom="polygon")
+            + geom_point(aes_string(colour="Gate"), size=2/(0.3*log(n)), alpha=0.4)
+            + theme_bw()
+            + guides(colour=guide_legend(override.aes = list(alpha=1)))
+            + ggtitle(paste(n, " cells", sep=""))
+      )
+    }
+    
+    
+    if (!is.null(xlim)){
+      if(scale_log){
+        p <- (p + scale_x_log10(limits=xlim, colnames(data2plot)[dims2plot[1]]))
+        if (!is.null(ylim)){
+          p <- (p + scale_y_log10(limits=ylim, colnames(data2plot)[dims2plot[2]]))
         }
-
-        data2plot <- data.frame(t(cytomatrix[dims2plot,]))
-        if(!is.null(colnames(data2plot)[dims2plot[1]]) &&
-               !is.null(colnames(data2plot)[dims2plot[2]])){
-            xname <- colnames(data2plot)[dims2plot[1]]
-            yname <- colnames(data2plot)[dims2plot[2]]
-            p <- ggplot(data2plot, aes_string(x=xname, y=yname))
-        }else{
-            colnames(data2plot) <- c("X", "Y")
-            p <- ggplot(data2plot, aes_string(x="X", y="Y"))
+      }
+      else{
+        p <- (p + xlim(xlim))
+        if (!is.null(ylim)){
+          p <- (p + ylim(ylim))
         }
-
-        p <- (p + geom_point(size=2/(0.3*log(n)), colour="blue")
-              + stat_density2d(aes_string(fill = "..level.."), alpha=0.8, geom="polygon")
-              + scale_fill_gradientn(colours=c("blue","green", "yellow", "red"),
-                                     name="Density")
-              + theme_bw()
+      }
+    }
+    if (!is.null(ylim) && is.null(xlim)){
+      if(scale_log){
+        p <- (p + scale_y_log10(limits=ylim, colnames(data2plot)[dims2plot[2]])
+              + scale_x_log10(colnames(data2plot)[dims2plot[1]])
         )
-
-
-        if (!is.null(xlim)){
-            if(scale_log){
-                p <- (p + scale_x_log10(limits=xlim, colnames(data2plot)[dims2plot[1]]))
-                if (!is.null(ylim)){
-                    p <- (p + scale_y_log10(limits=ylim, colnames(data2plot)[dims2plot[2]]))
-                }
-            }
-            else{
-                p <- (p + xlim(xlim))
-                if (!is.null(ylim)){
-                    p <- (p + ylim(ylim))
-                }
-            }
-        }
-        if (!is.null(ylim) && is.null(xlim)){
-            if(scale_log){
-                p <- (p + scale_y_log10(limits=ylim, colnames(data2plot)[dims2plot[2]])
-                      + scale_x_log10(colnames(data2plot)[dims2plot[1]])
-                )
-            }
-            else{
-                p <- (p + ylim(ylim))
-            }
-        }
-
-        if(scale_log && is.null(ylim) && is.null(xlim)){
-            p <- (p + scale_x_log10(colnames(data2plot)[dims2plot[1]])
-                  + scale_y_log10(colnames(data2plot)[dims2plot[2]])
-            )
-        }
+      }
+      else{
+        p <- (p + ylim(ylim))
+      }
     }
-
-    for (a in gg.add) {
-        p <- p + a
+    
+    if(scale_log && is.null(ylim) && is.null(xlim)){
+      p <- (p + scale_x_log10(colnames(data2plot)[dims2plot[1]])
+            + scale_y_log10(colnames(data2plot)[dims2plot[2]])
+      )
     }
-    print(p)
+  }
+  
+  for (a in gg.add) {
+    p <- p + a
+  }
+  print(p)
 }
