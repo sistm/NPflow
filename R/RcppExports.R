8bfb865d7a7254df553a36a10ddd8d676c94c4f1
Boris <boris.hejblum.ensai@gmail.com>
2017-08-23 03:14:39 +0200

    updating Rcpp

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -71,6 +71,43 @@ Fmeasure_costC <- function(c) {
     .Call(`_NPflow_Fmeasure_costC`, c)
 }
 
+#' C++ implementation of similarity matrix computation using precomputed distances
+#'
+#'
+#'@param c an MCMC partitions of length \code{n}.
+#'
+#'@param d a symmetric \code{n x n} matrix containing distances
+#'between each group distributions.
+#'
+#'@author Boris Hejblum, Chariff Alkhassim
+#'
+#'@export
+#'
+#'@examples
+#'c <- c(1,1,2,3,2,3)
+#'d <- matrix(runif(length(c)^2),length(c))
+#'NuMatParC(c,d)
+#'
+#'
+NuMatParC <- function(c, d) {
+    .Call(`_NPflow_NuMatParC`, c, d)
+}
+
+#' C++ implementation of residual trace computation step used when sampling the scale
+#'
+#'@param eps a numeric matrix where each column contains the centered and unskewed observations
+#'@param sigma a numeric covariance matrix
+#'
+#'@return the computed trace
+#'
+#'@keywords internal
+#'
+#'@export
+#'
+traceEpsC <- function(eps, sigma) {
+    .Call(`_NPflow_traceEpsC`, eps, sigma)
+}
+
 #' C++ implementation of multivariate Normal inverse Wishart probability density function for multiple inputs
 #'
 #'@param Mu data matrix of dimension \code{p x n}, \code{p} being the dimension of the
@@ -447,28 +484,6 @@ mvstlikC <- function(x, c, clustval, xi, psi, sigma, df, loglik = TRUE) {
     .Call(`_NPflow_mvstlikC`, x, c, clustval, xi, psi, sigma, df, loglik)
 }
 
-#' C++ implementation of similarity matrix computation using precomputed distances
-#'
-#'
-#'@param c an MCMC partitions of length \code{n}.
-#'
-#'@param d a symmetric \code{n x n} matrix containing distances
-#'between each group distributions.
-#'
-#'@author Boris Hejblum, Chariff Alkhassim
-#'
-#'@export
-#'
-#'@examples
-#'c <- c(1,1,2,3,2,3)
-#'d <- matrix(runif(length(c)^2),length(c))
-#'NuMatParC(c,d)
-#'
-#'
-NuMatParC <- function(c, d) {
-    .Call(`_NPflow_NuMatParC`, c, d)
-}
-
 #' C++ implementation of the multinomial sampling from a matrix
 #' of column vectors, each containing the sampling probabilities
 #' for their respective draw
@@ -489,6 +504,27 @@ NuMatParC <- function(c, d) {
 #'
 sampleClassC <- function(probMat, Log = FALSE) {
     .Call(`_NPflow_sampleClassC`, probMat, Log)
+}
+
+#' C++ implementation
+#'
+#'
+#'@param cc a matrix whose columns each represents a (MCMC) partition
+#'
+#'@export
+#'
+#'@examples
+#'c <- list(c(1,1,2,3,2,3), c(1,1,1,2,3,3),c(2,2,1,1,1,1))
+#'similarityMatC(sapply(c, "["))
+#'
+#'c2 <- list()
+#'for(i in 1:10){
+#'     c2 <- c(c2, list(rmultinom(n=1, size=200, prob=rexp(n=200))))
+#'}
+#'similarityMatC(sapply(c2, "["))
+#'
+similarityMatC <- function(cc) {
+    .Call(`_NPflow_similarityMatC`, cc)
 }
 
 #' C++ implementation
@@ -517,42 +553,6 @@ sampleClassC <- function(probMat, Log = FALSE) {
 #'}
 similarityMat_nocostC <- function(cc) {
     .Call(`_NPflow_similarityMat_nocostC`, cc)
-}
-
-#' C++ implementation
-#'
-#'
-#'@param cc a matrix whose columns each represents a (MCMC) partition
-#'
-#'@export
-#'
-#'@examples
-#'c <- list(c(1,1,2,3,2,3), c(1,1,1,2,3,3),c(2,2,1,1,1,1))
-#'similarityMatC(sapply(c, "["))
-#'
-#'c2 <- list()
-#'for(i in 1:10){
-#'     c2 <- c(c2, list(rmultinom(n=1, size=200, prob=rexp(n=200))))
-#'}
-#'similarityMatC(sapply(c2, "["))
-#'
-similarityMatC <- function(cc) {
-    .Call(`_NPflow_similarityMatC`, cc)
-}
-
-#' C++ implementation of residual trace computation step used when sampling the scale
-#'
-#'@param eps a numeric matrix where each column contains the centered and unskewed observations
-#'@param sigma a numeric covariance matrix
-#'
-#'@return the computed trace
-#'
-#'@keywords internal
-#'
-#'@export
-#'
-traceEpsC <- function(eps, sigma) {
-    .Call(`_NPflow_traceEpsC`, eps, sigma)
 }
 
 #' C++ implementation
